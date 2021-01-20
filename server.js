77e3a713ffaeae3c8e3969ec7c30b96330b1420c
UlisesPeguero <peguero.ulises@gmail.com>
2021-01-20 16:07:55 -0600

    Express setup ready

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const api = express.Router();
+
+dotenv.config(); // load .env 
+
+const PORT = process.env.HTTP_PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// public files
+app.use(express.static('public'));
+
+// app.use('/api', routerApi);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
