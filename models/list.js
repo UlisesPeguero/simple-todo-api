74a63da2c2601923053ed3a358b9bbde208927fb
Ulises Peguero <peguero.ulises@gmail.com>
2021-01-20 21:02:48 -0600

    Created base folders

---
+++ models/list.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+
+});
+
+module.exports = schema;
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+// routers
+const todosRouter = require('./routes/todos');
+const listsRouter = require('./routes/lists');
+//
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config(); // load .env 
 
+// database connection and setup
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connection to the database was succesful.');
+});
+
+// express setup
 const PORT = process.env.HTTP_PORT;
 const app = express();
 
@@ -13,6 +31,7 @@ app.use(express.json());
 // public files
 app.use(express.static('public'));
 
+// routers
 // app.use('/api', routerApi);
 
 app.listen(PORT, () => {
